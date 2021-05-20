d09a227e374dab706c4a6243dccb8144e844959c
Onur Dereli <onur.dereli@msuyazilim.com>
2021-05-20 15:27:57 +0300

    Add project files.

---
+++ RabbitMQ.publisher/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Channels;
+using RabbitMQ.Client;
+
+namespace RabbitMQ.publisher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var connectionFactory = new ConnectionFactory
+            {
+                HostName = "localhost"
+            };
+
+            using var connection = connectionFactory.CreateConnection();
+
+            var model = connection.CreateModel();
+
+            var helloQueue = "hello-queue";
+
+            model.QueueDeclare(helloQueue, true, false, false);
+
+            Enumerable.Range(1, 50).ToList().ForEach(x =>
+            {
+                // mesajlar byte dizin olarak alınır
+                string message = $"Message {x}";
+                var messageBody = Encoding.UTF8.GetBytes(message);
+
+                model.BasicPublish(string.Empty, helloQueue, null, messageBody);
+
+                Console.WriteLine($"Mesaj gönderilmiştir. {message}");
+            });
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ RabbitMQ.subscriber/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Text;
+using System.Threading;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace RabbitMQ.subscriber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var connectionFactory = new ConnectionFactory
+            {
+                HostName = "localhost"
+            };
+
+            using var connection = connectionFactory.CreateConnection();
+
+            var model = connection.CreateModel();
+
+            var helloQueue = "hello-queue";
+            //model.QueueDeclare(helloQueue, true, false, false);
+            //global true; subscriberlara toplamda kaça bölüneceğini belirtir
+            model.BasicQos(0, 1, false);
+
+            var consumer = new EventingBasicConsumer(model);
+            //autoAck = kuyruktan bir mesajı gönderildiğinde direk siliyor, false işleminde haberleşme bittikten sonra kontrolllü silinmeyi sağlar
+            model.BasicConsume(helloQueue, false, consumer);
+
+            consumer.Received += (sender, eventArgs) =>
+            {
+                var message = Encoding.UTF8.GetString(eventArgs.Body.ToArray());
+                Thread.Sleep(1500);
+                Console.WriteLine("Gelen Mesaj: " + message);
+
+                model.BasicAck(eventArgs.DeliveryTag, false);
+            };
+            Console.ReadLine();
+        }
+    }
+}
